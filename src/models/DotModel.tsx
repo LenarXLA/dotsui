61786dd3bec6c10e3fb89374540221e85451ddce
Lenar <53114683+LenarXLA@users.noreply.github.com>
2023-10-23 15:06:33 +0500

    Update DotModel.tsx

--- src/models/DotModel.tsx
+++ src/models/DotModel.tsx
@@ -18,29 +18,3 @@ export class DotModel {
       this.comments = comments;
     }
 }
-
-
-{/* <Circle
-key={dot.id}
-id={dot.id}
-x={dot.x}
-y={dot.y}
-radius={dot.radius} 
-fill={dot.color} */}
-// numPoints={5}
-// innerRadius={20}
-// outerRadius={40}
-// fill="#89b717"
-// opacity={0.8}
-// draggable
-// rotation={star.rotation}
-// shadowColor="black"
-// shadowBlur={10}
-// shadowOpacity={0.6}
-// shadowOffsetX={star.isDragging ? 10 : 5}
-// shadowOffsetY={star.isDragging ? 10 : 5}
-// scaleX={star.isDragging ? 1.2 : 1}
-// scaleY={star.isDragging ? 1.2 : 1}
-// onDragStart={handleDragStart}
-// onDragEnd={handleDragEnd}
-// />
